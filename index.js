c5d2c4e55349b1dd0d29ce590b24f769db56f566
Hossain-A1 <mrhossainahmed7@gmail.com>
2023-06-14 19:43:34 +0300

    admin access done

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 const userRoutes = require("./routes/user")
+const deviceRoutes = require("./routes/device")
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -21,6 +22,7 @@ app.get("/", (req, res) => {
   res.status(200).json({ message: "hello hossain" });
 });
 app.use("/api/auth/user",userRoutes )
+app.use("/api/data/device",deviceRoutes )
 // mongoDB connected
 mongoose.connect(uri, { useUnifiedTopology: true }).then(() => {
   app.listen(port, () => {
